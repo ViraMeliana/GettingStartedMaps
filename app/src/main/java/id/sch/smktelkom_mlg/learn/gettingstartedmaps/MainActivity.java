1d1dc137d1cc771ad5d922781a00bf900379ece2
Vira Meliana Agustin <viramelianaagustin@gmail.com>
2017-10-29 10:06:47 +0700

    Coordinates

--- app/src/main/java/id/sch/smktelkom_mlg/learn/gettingstartedmaps/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/gettingstartedmaps/MainActivity.java
@@ -17,36 +17,63 @@ import com.google.android.gms.maps.model.LatLng;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    static final CameraPosition TBN = CameraPosition.builder()
+            .target(new LatLng(1.2836, 103.8604))
+            .zoom(17)
+            .bearing(90)
+            .tilt(45)
+            .build();
+    static final CameraPosition PAPUA = CameraPosition.builder()
+            .target(new LatLng(48.8584, 2.2945))
+            .zoom(17)
+            .bearing(0)
+            .tilt(45)
+            .build();
+    static final CameraPosition BALI = CameraPosition.builder()
+            .target(new LatLng(37.5665, 126.9780))
+            .zoom(17)
+            .bearing(0)
+            .tilt(45)
+            .build();
+    static final CameraPosition JKT = CameraPosition.builder()
+            .target(new LatLng(52.3702, 4.8952))
+            .zoom(17)
+            .bearing(90)
+            .tilt(45)
+            .build();
+
     GoogleMap m_map;
     boolean mapReady = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btnMap = (Button) findViewById(R.id.btnMap);
-        btnMap.setOnClickListener(new View.OnClickListener() {
+
+        Button btnPapua = (Button) findViewById(R.id.btnPapua);
+        btnPapua.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (mapReady)
-                    m_map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                    flyTo(PAPUA);
             }
         });
 
-        Button btnSatellite = (Button) findViewById(R.id.btnSatellite);
-        btnSatellite.setOnClickListener(new View.OnClickListener() {
+        Button btnBali = (Button) findViewById(R.id.btnBali);
+        btnBali.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (mapReady)
-                    m_map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                    flyTo(BALI);
             }
         });
 
-        Button btnHybrid = (Button) findViewById(R.id.btnHybrid);
-        btnHybrid.setOnClickListener(new View.OnClickListener() {
+        Button btnJkt = (Button) findViewById(R.id.btnJkt);
+        btnJkt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (mapReady)
-                    m_map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                    flyTo(JKT);
             }
         });
 
@@ -61,9 +88,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         toast.show();
         mapReady = true;
         m_map = map;
-        LatLng smktelkom = new LatLng(-7.976830, 112.658958);
-        CameraPosition target = CameraPosition.builder().target(smktelkom).zoom(14).build();
-        m_map.moveCamera(CameraUpdateFactory.newCameraPosition(target));
+        m_map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        flyTo(TBN);
 
     }
 
@@ -71,7 +97,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
-    }
+            }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -82,5 +108,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
 
         return super.onOptionsItemSelected(item);
+            }
+
+    private void flyTo(CameraPosition target) {
+        m_map.animateCamera(CameraUpdateFactory.newCameraPosition(target), 10000, null);
     }
 }
