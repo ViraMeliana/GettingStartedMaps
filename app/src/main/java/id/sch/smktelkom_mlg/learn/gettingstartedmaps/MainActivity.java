519a37338b8170e72b13e058422511ec9cf2a0aa
Vira Meliana Agustin <viramelianaagustin@gmail.com>
2017-11-03 18:51:54 +0700

    DrawCircles

--- app/src/main/java/id/sch/smktelkom_mlg/learn/gettingstartedmaps/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/gettingstartedmaps/MainActivity.java
@@ -1,6 +1,6 @@
 package id.sch.smktelkom_mlg.learn.gettingstartedmaps;
 
-import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -11,83 +11,34 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    static final CameraPosition JTG = CameraPosition.builder()
-            // .target(new LatLng(-6.858623, 111.642224))
+    static final CameraPosition VIR = CameraPosition.builder()
             .target(new LatLng(48.8584, 2.2945))
             .zoom(15)
             .bearing(0)
             .tilt(45)
             .build();
-
     GoogleMap m_map;
     boolean mapReady = false;
-    MarkerOptions house;
-    MarkerOptions quba;
-    MarkerOptions pasar;
-    MarkerOptions kec;
-    MarkerOptions koramil;
-    MarkerOptions smp;
-
-    @Override
-    public Resources getResources() {
-        return super.getResources();
-    }
+    LatLng renton = new LatLng(-8.400375, 114.273540);
+    LatLng kirkland = new LatLng(47.7301986, -122.1768858);
+    LatLng everett = new LatLng(47.978748, -122.202001);
+    LatLng lynwood = new LatLng(47.819533, -122.32288);
+    LatLng montlake = new LatLng(47.7973733, -122.3281771);
+    LatLng kent = new LatLng(47.385938, -122.258212);
+    LatLng showare = new LatLng(47.38702, -122.23986);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        house = new MarkerOptions()
-//                .position(new LatLng(-6.886892, 111.654675))
-//                .title("My House")
-                .position(new LatLng(48.8584, 2.2945))
-                .title("First Place")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_location_on_black_24dp));
-
-        quba = new MarkerOptions()
-//                .position(new LatLng(-6.887492, 111.656472))
-//                .title("Masjid Ar-rahmah")
-                .position(new LatLng(48.8584, 2.2949))
-                .title("Second Place")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_location_on_black_24dp));
-
-        pasar = new MarkerOptions()
-//                .position(new LatLng(-6.879152, 111.658757))
-//                .title("Pasar Jatirogo")
-                .position(new LatLng(48.8584, 2.2947))
-                .title("Third Place")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_location_on_black_24dp));
-
-        kec = new MarkerOptions()
-//                .position(new LatLng(-6.885809, 111.658339))
-//                .title("Kantor Kec. Jatirogo")
-                .position(new LatLng(48.8584, 2.2942))
-                .title("Fourth Place")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_location_on_black_24dp));
-
-        koramil = new MarkerOptions()
-//                .position(new LatLng(-6.888232, 111.662722))
-//                .title("Koramil Jatirogo")
-                .position(new LatLng(48.8584, 2.29451))
-                .title("Fifth Place")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_location_on_black_24dp));
-
-        smp = new MarkerOptions()
-//                .position(new LatLng(-6.884760, 111.657529))
-//                .title("SMPN 1 Jatirogo")
-                .position(new LatLng(48.8584, 2.2950))
-                .title("Sixth Place")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_location_on_black_24dp));
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
@@ -97,22 +48,21 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap map) {
         Toast toast = Toast.makeText(getApplicationContext(), "Map Ready!", Toast.LENGTH_SHORT);
         toast.show();
-        mapReady = true;
-        m_map = map;
-        m_map.addMarker(house);
-        m_map.addMarker(quba);
-        m_map.addMarker(pasar);
-        m_map.addMarker(kec);
-        m_map.addMarker(koramil);
-        m_map.addMarker(smp);
-        flyTo(JTG);
+        map.moveCamera(CameraUpdateFactory.newCameraPosition(VIR));
+        //map.addPolyline(new PolylineOptions().geodesic(true).add(renton).add(kirkland).add(everett).add(lynnwood).add(montlake).add(kent).add(showare).add(renton));
+        //map.addPolygon(new PolygonOptions().add(renton, kirkland, everett, lynnwood).fillColor(Color.GREEN));
+        map.addCircle(new CircleOptions()
+                .center(renton)
+                .radius(200)
+                .strokeColor(Color.GREEN)
+                .fillColor(Color.argb(64, 0, 255, 0)));
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
-            }
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -121,10 +71,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         if (id == R.id.action_settings) {
             return true;
         }
-        return super.onOptionsItemSelected(item);
-            }
 
-    private void flyTo(CameraPosition target) {
-        m_map.animateCamera(CameraUpdateFactory.newCameraPosition(target), 10000, null);
+        return super.onOptionsItemSelected(item);
     }
 }
